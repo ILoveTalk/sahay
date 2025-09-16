9140a8ef643f7c84bbc72c442f43d701360869f5
Mr Deepak <devgrowr@gmail.com>
2025-09-16 17:52:44 +0530

    Update geminiService.ts

--- services/geminiService.ts
+++ services/geminiService.ts
@@ -1,12 +1,7 @@
 
 import { GoogleGenAI, Chat } from "@google/genai";
 
-// Ensure the API key is available.
-if (!process.env.API_KEY) {
-  throw new Error("API_KEY environment variable is not set.");
-}
-
-const ai = new GoogleGenAI({ apiKey: process.env.API_KEY });
+const ai = new GoogleGenAI({ "AIzaSyAkjNedkc0jkbPuiG0OqT6d7V0aKVDSUyk" });
 
 const chat: Chat = ai.chats.create({
   model: 'gemini-2.5-flash',
