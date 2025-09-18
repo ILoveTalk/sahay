c86b700c3e0cebbba092667982c8586361368949
Mr Deepak <devgrowr@gmail.com>
2025-09-18 09:38:09 +0530

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -3,7 +3,7 @@ import LeafIcon from './icons/LeafIcon';
 
 const Logo: React.FC = () => (
   <div className="flex items-center gap-3">
-    <img src="https://raw.githubusercontent.com/ILoveTalk/sahay/main/assets/logo.png" alt="Sahay Logo" className="w-8 h-8" />
+    <img src="https://raw.githubusercontent.com/ILoveTalk/sahay/refs/heads/main/assets/logo.png" alt="Sahay Logo" className="w-8 h-8" />
     <span className="text-3xl font-['Poppins'] font-semibold text-[#343A40]">
       Sahay
     </span>
